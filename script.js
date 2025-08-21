11dad07a841603ee78335b0f67c5821391bd765b
daramsreya83@gmail.com <your.email@example.com>
2025-08-21 20:31:48 +0530

    Assigment

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+    sounds.forEach(sound => {
+      const btn = document.querySelector(`.btn:nth-child(${sounds.indexOf(sound)+1})`);
+      
+      btn.addEventListener('click', () => {
+        stopSongs();
+        const audio = new Audio(`sounds/${sound}.mp3`);
+        audio.play();
+        window.currentAudio = audio; // store current audio globally
+      });
+    });
+
+    // stop button
+    document.querySelector('.stop').addEventListener('click', stopSongs);
+
+    function stopSongs() {
+      if (window.currentAudio) {
+        window.currentAudio.pause();
+        window.currentAudio.currentTime = 0;
+      }
+    }
